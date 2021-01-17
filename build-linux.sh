3075a04be4037c0b6d66700b8d0de10b7c6480ab
kennyfrc <fxkennyfrc@gmail.com>
2021-01-17 15:40:55 +0800

    ubuntu build script updated

--- build-linux.sh
+++ build-linux.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-sudo apt-get install cmake
-sudo apt-get install libboost-all-dev
+sudo apt-get update
+sudo apt-get install -y gcc g++
+sudo apt-get install -y cmake
+sudo apt-get install -y libboost-all-dev
 git submodule sync --recursive
 git submodule update --recursive --init
 cmake CMakeLists.txt
